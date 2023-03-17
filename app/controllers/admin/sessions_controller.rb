03afc222d6c7719b556fb135d83f1a1a4fd84c62
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-03-17 05:06:05 +0000

    [fix]admin,customer session,registration編集

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +25,19 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+  
+  protected
+  
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  end 
+  
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +25,19 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    new_customer_session_path
+  end
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  end
+  
 end
