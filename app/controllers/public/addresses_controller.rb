73a61d853597c920b596a6de3eec7772a530a8fe
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-03-20 05:25:38 +0000

    [fix]migrate修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,6 +1,7 @@
 class Public::AddressesController < ApplicationController
   # 配送先登録/一覧画面
   def index
+    @addresses = Address.all
   end
 
   # 配送先編集画面
@@ -18,5 +19,10 @@ class Public::AddressesController < ApplicationController
   # 配送先の削除
   def destroy
   end
+  
+  private
+  
+  def address_params
+    params.require(:address),permit(:customer_id,:name,:postal_code,:address)
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2023_03_13_040719) do
     t.integer "customer_id"
     t.string "name"
     t.string "postal_code"
-    t.string "addresse"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
