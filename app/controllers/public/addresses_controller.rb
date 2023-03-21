2d991dbd9143dddb8f079e7e726d88f317db8e3c
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-03-21 04:13:11 +0000

    [fix]views/public/address 閲覧制限追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,9 @@
 class Public::AddressesController < ApplicationController
+  
   # 配送先登録/一覧画面
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = Address.where(customer_id: current_customer.id)
   end
 
   def create
@@ -10,17 +11,25 @@ class Public::AddressesController < ApplicationController
     if @address.save!
       flash[:notice]="配送先を登録しました"
       redirect_to addresses_path
-    # else
-      # render:index
+    else
+      render:index
     end
   end
 
   # 配送先編集画面
   def edit
+    @address = Address.find(params[:id])
   end
 
   # 配送先の更新
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice]="配送先の変更が保存されました"
+      redirect_to addresses_path
+    else
+      render:index
+    end
   end
 
   # 配送先の削除
