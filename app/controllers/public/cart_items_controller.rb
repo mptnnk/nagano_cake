a792c5691c72d487383bb8dea7bd7a02f6407f21
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-03-22 06:15:43 +0000

    カート機能調整中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,24 +1,25 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
   # カート内商品一覧画面（数量変更・カート削除の要素を含む）
   def index
     @cart_item = CartItem.new
     # @cart_items = CartItem.where(customer_id: current_customer.id)
-    @cart_items = current_customer.cart_items.all
+    @cart_items = current_customer.cart_items
   end
   
   # カート内商品データ追加
   def create
-    @cart_item = current_customer.cart_items.build(cart_item_params)
-    @cart_items = current_customer.cart_items.all
-    @cart_items.each do |cart_item|
-      if cart_item.item_id == @cart_item.item_id
-        new_amount = cart_item.amount + @cart_item.amount
-        cart_item.update_attribute(:amount, new_amount)
-        @cart_item.delete
-      end
-    @cart_item.save
-    redirect_to(cart_items_path)and return
-    end
+    # @cart_item = current_customer.cart_items.build(cart_item_params)
+    # @cart_items = current_customer.cart_items
+    # @cart_items.each do |cart_item|
+    #   if cart_item.item_id == @cart_item.item_id
+    #     new_amount = cart_item.amount + @cart_item.amount
+    #     cart_item.update_attribute(:amount, new_amount)
+    #     @cart_item.delete
+    # #   end
+    # @cart_item.save
+    # redirect_to(cart_items_path)and return
+    # end
   end
   
   # カート内商品データ更新
@@ -42,8 +43,7 @@ class Public::CartItemsController < ApplicationController
   # カート内商品データ削除（全て）
   def destroy_all
     # @cart_items = CartItem.where(customer_id: current_customer.id)
-    @cart_items = current_customer.cart_items.all
-    @cart_items.destroy
+    current_customer.cart_items.destroy_all
     flash[:notice]="注文商品をすべて削除しました"
     redirect_to cart_items_path
   end
