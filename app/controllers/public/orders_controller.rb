a8671b8cfb4e48fa85f7b6b8014b04baf93c2585
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-03-27 05:43:18 +0000

    [add]views/public/orders new,confirm 調整中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,5 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
   
   # 注文情報入力画面（支払方法・配送先選択）
   def new
@@ -13,7 +14,24 @@ class Public::OrdersController < ApplicationController
 
   # 注文情報確認画面
   def confirm
+    @cart_items = current_customer.cart_items
     @order = Order.new(order_params)
+    @order.payment_method = params[:order][:payment_method]
+    
+    @order.shipping_cost = 800
+    
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+      
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+      
+    end
   end
 
   # 注文完了画面
