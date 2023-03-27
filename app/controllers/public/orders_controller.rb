b38dac2914b0740db2d2cb6694102c75c990d37d
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-03-27 03:11:07 +0000

    [add]views/public/orders new　調整中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,15 +1,19 @@
 class Public::OrdersController < ApplicationController
+  
   # 注文情報入力画面（支払方法・配送先選択）
   def new
-    @order = Order.new
-  end
-  
-  def create
-    @order = Order.new(params[:order_params])
+    if current_customer.cart_items.blank?
+      flash[:alert]="注文する商品をカートに入れてください"
+      redirect_to root_path
+    else
+      @order = Order.new
+      @customer = current_customer
+    end
   end
 
   # 注文情報確認画面
   def confirm
+    @order = Order.new(order_params)
   end
 
   # 注文完了画面
@@ -18,10 +22,12 @@ class Public::OrdersController < ApplicationController
 
   # 注文確定処理
   def create
+    @order = Order.new(params[:order_params])
   end
 
   # 注文履歴画面
   def index
+    @orders = current_customer.orders.all
   end
 
   # 注文履歴詳細画面
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ApplicationRecord
   belongs_to:customer
   validates :customer_id, presence: true
+  
+  def address_display
+    '〒' + postal_code + ' ' + address + ' ' + name
+  end
 end
