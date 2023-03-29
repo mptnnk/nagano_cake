c7b41aedfde2e7b3137c64f30baf0e63c9165ce0
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-03-29 07:48:23 +0000

    [fix]登録〜注文確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
 
   validates:is_active,inclusion:[true,false]
 
-  has_one_attached :image
+  has_one_attached :cake_image
 
   def with_tax_price
     (price*1.1).floor
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
   # floorで小数点以下を切り捨てる
 
   def get_image
-    unless image.attached?
-    file_path = Rails.root.join('app/assets/images/default-image.jpg')
-    image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpg')
+    unless cake_image.attached?
+     file_path = Rails.root.join('app/assets/images/default-image.jpg')
+     cake_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpg')
     end
-    image
+    cake_image
   end
 
 end
