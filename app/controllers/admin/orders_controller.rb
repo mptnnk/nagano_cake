f8726e9769cf12c28210631fc8e7abe614f50907
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-03-31 06:11:18 +0000

    [add]views/admin/orders show編集中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,9 +4,24 @@ class Admin::OrdersController < ApplicationController
   def show
     @orders = Order.all
     @order = Order.find(params[:id])
+    @customer = @order.customer
   end
   
   # 注文ステータスの更新
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash[:notice]="注文ステータスを更新しました"
+      redirect_to admin_order_path(@order.id)
+    else
+      render:show
+    end
   end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
