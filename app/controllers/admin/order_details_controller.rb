aa77d5a1e64b30166b7f7321e44d9ec8372b5d53
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-04-01 11:00:09 +0000

    [add]views/admin/order_detail ステータス更新追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,5 +2,20 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
   # 制作ステータスの更新
   def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    if @order_detail.update(order_detail_params)
+      flash[:notice]="制作ステータスを更新しました"
+      redirect_to admin_order_path(@order.id)
+    else
+      flash[:alert]="制作ステータスの更新に失敗しました"
+      redirect_to admin_order_path(@order.id)
+    end
   end
+  
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,15 +5,18 @@ class Admin::OrdersController < ApplicationController
     @orders = Order.all
     @order = Order.find(params[:id])
     @customer = @order.customer
+    @order_details = @order.order_details
   end
   
   # 注文ステータスの更新
   def update
     @order = Order.find(params[:id])
+    @order_detail = OrderDeatil.find(@order.order_detail)
     if @order.update(order_params)
       flash[:notice]="注文ステータスを更新しました"
       redirect_to admin_order_path(@order.id)
     else
+      flash[:alert]="注文ステータスの更新に失敗しました"
       render:show
     end
   end
