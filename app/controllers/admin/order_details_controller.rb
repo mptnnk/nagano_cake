25f5eb1671791a53b360bcb92c60dda3c8fc252d
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-04-01 12:09:34 +0000

    [add]views/admin/orders ステータス更新調整中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,14 +1,21 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
   # 制作ステータスの更新
+  # order_detail 2 in_production / order 2 in_production
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
-    if @order_detail.update(order_detail_params)
-      flash[:notice]="制作ステータスを更新しました"
+    
+    @order_detail.update(order_detail_params)
+    if @order_detail.making_status == "in_production"
+      @order.update(status: 2)
+      flash[:notice]="製作ステータスを更新しました"
+      redirect_to admin_order_path(@order.id)
+    elsif @order.order_details.count == @order.order_details.where(making_status: "production_comp").count
+      @order.update(status: 3)
+      flash[:notice]="ステータスを発送準備中に更新しました"
       redirect_to admin_order_path(@order.id)
     else
-      flash[:alert]="制作ステータスの更新に失敗しました"
       redirect_to admin_order_path(@order.id)
     end
   end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,16 +11,18 @@ class Admin::OrdersController < ApplicationController
   # 注文ステータスの更新
   def update
     @order = Order.find(params[:id])
-    @order_detail = OrderDeatil.find(@order.order_detail)
+    @order_details = @order.order_details
+    
     if @order.update(order_params)
       flash[:notice]="注文ステータスを更新しました"
+      @order_details.update_all(making_status: 1) if @order.status == "payment_confirmation"
       redirect_to admin_order_path(@order.id)
     else
       flash[:alert]="注文ステータスの更新に失敗しました"
       render:show
     end
   end
-  
+
   private
   
   def order_params
