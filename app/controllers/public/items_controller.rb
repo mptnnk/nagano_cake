ebef4631ef7d22c50544245ffdaa8df8452a1a42
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-04-02 02:37:23 +0000

    [add]検索機能調整中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,20 @@
 class Public::ItemsController < ApplicationController
   # 商品一覧
   def index
-    @items = Item.all
+    @genres = Genre.all
+    # @items = Item.all
+    if params[:genre_id]
+      @item_genre = Genre.find(params[:gernre_id]).items
+      @items = @item_genre.page(params[:page])
+      @items_count = @items_genre.count
+    elsif params[:search]
+      @search = Item.search(params[:search])
+      @items = @search.page(params[:page])
+      @items_count = @items.count
+    else
+      @items = Item.page(params[:page])
+      @items_count = Item.count
+    end
   end
 
   # 商品詳細
