c432d8cef106f3d1e012631025106db7db4145f4
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-04-04 03:40:26 +0000

    [fix]レイアウト調整

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,8 @@
 class Address < ApplicationRecord
   belongs_to:customer
   validates :customer_id, presence: true
-  
+  validates :postal_code, length: {minimum:7, maximum:7}
+
   def address_display
     '〒' + postal_code + ' ' + address + ' ' + name
   end
