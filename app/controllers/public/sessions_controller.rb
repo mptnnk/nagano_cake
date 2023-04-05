79cdb13c548780a3e30950316478fae519e7e741
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-04-05 12:55:39 +0000

    [fix]レイアウト調整

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -26,21 +26,21 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
+
   def after_sign_in_path_for(resource)
     root_path
   end
-  
+
   def after_sign_out_path_for(resource)
-    new_customer_session_path
+    root_path
   end
-  
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
   end
-  
+
   def customer_state
   # 退会しているかを判断するメソッド
     @customer = Customer.find_by(email: params[:customer][:email])
@@ -53,5 +53,5 @@ class Public::SessionsController < Devise::SessionsController
       redirect_to new_customer_registration_path
     end
   end
-  
+
 end
